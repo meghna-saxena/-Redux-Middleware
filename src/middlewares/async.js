7ef37dfdf2bde5db04b8295cd17432172f201e1b
meghna-saxena <meghna.s1906@gmail.com>
2017-10-04 19:34:21 +0530

    action with no promise

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,7 +1,8 @@
 export default function ({dispatch}) {
     return next => action => {
-        console.log(action);
-
-        next(action);
+        if (!action.payload || !action.payload.then) {
+            return next(action);
+        }
+        console.log('We dont have a promise', action);
     };
 }
