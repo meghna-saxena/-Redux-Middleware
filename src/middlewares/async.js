e02ff6b48983ef9f0217fdc27cdd391bb509ee12
meghna-saxena <meghna.s1906@gmail.com>
2017-10-04 20:38:27 +0530

    reviewed the code

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,13 +1,18 @@
 export default function ({dispatch}) {
+    //if action doesnt has payload or .then property,
+    // we can return the action to next reducer
     return next => action => {
         if (!action.payload || !action.payload.then) {
             return next(action);
         }
-
+//when we have promise, make sure it gets resolved
         action.payload
             .then (function(response) {
+                //create new action creator with old 'type' 
+                //but payload of response data
                 const newAction = {...action, payload:response};
+                //forward new action to all the middlewares again
                 dispatch(newAction);
-            });
+            });s
     };
 }
